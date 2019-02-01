4843aa3952a77a3468f324949eb97b61d80b19a5
sunny-selector-0188 <sonya_dumitru@yahoo.com>
2019-02-01 03:11:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,28 +5,20 @@ function updateObjectWithKeyAndValue(object,key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object,key, value) {
-<<<<<<< HEAD
   object[key] = value;
   return object;
 }
 function deleteFromObjectByKey(object, key) {
   var cloneObject = Object.assign({}, object, cloneObject);
-=======
   objects[key] = value;
   return object;
 }
 function deleteFromObjectByKey(object, key) {
   var cloneObject = Object.assign({}, object);
->>>>>>> 44ecbd7e5a382c1c4e08baefef918ca49e7de332
   delete cloneObject[key];
   return cloneObject;
 }
-
-<<<<<<< HEAD
 function destructivelyDeleteFromObjectByKey(object, key) {
-=======
-function destructivelyUpdateObjectWithKeyAndValue(object, key) {
->>>>>>> 44ecbd7e5a382c1c4e08baefef918ca49e7de332
   delete object[key];
   return object;
 }
